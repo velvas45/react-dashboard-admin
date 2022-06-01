b80e3626b3e40dee0da27f5585f0b34d9126eb30
helmi <helmi@ddtc.co.id>
2022-06-01 16:33:06 +0700

    add homepage page

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const SparkLine = () => {
-  return <div>SparkLine</div>;
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      LineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      type={type}
+      dataSource={data}
+      xName="xVal"
+      yName="yVal"
+      tooltipSettings={{
+        visible: true,
+        // eslint-disable-next-line no-undef
+        // eslint-disable-next-line no-template-curly-in-string
+        format: "${xVal} : data ${yVal}",
+        trackLineSettings: {
+          visible: true,
+        },
+      }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
 };
 
 export default SparkLine;
