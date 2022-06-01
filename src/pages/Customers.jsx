085e6adbe346ca0c009b4d2698737ba6d622cc3c
helmi <helmi@ddtc.co.id>
2022-06-01 18:46:19 +0700

    add tables functionality

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Toolbar,
+  Selection,
+  Edit,
+  Sort,
+  Filter,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
+import { customersData, customersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Customers = () => {
-  return <div>Customers</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        dataSource={customersData}
+        toolbar={["Delete"]}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        width="auto"
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Selection, Edit, Sort, Filter, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Customers;
